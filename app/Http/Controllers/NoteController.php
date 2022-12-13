51bdc691811323aff126662cd26d1ebd26bb5a53
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-13 14:28:59 +0000

    Implemented read all notes

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Exception;
+use Illuminate\Http\Request;
+
+class NoteController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        try {
+            $notes = Note::all();
+
+            return $this->sendResponse($notes, "Notes Retrieved Successfully");
+            
+        } catch (Exception $exception) {
+            return $this->sendError($exception->getMessage(), $exception, 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Note $note)
+    {
+        //
+    }
+}
