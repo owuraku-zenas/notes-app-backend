004b78fb2fbcf30e25e49782d4c261294c846d52
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-13 14:27:50 +0000

    Created Note Model, Migration, Generated fake data

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'title' => $this->faker->unique()->sentence(),
+            'description' => $this->faker->text(500),
+        ];
+    }
+}
